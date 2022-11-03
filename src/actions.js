f73244ea3e27b30fcf7cf087af2b1f67fa006e32
Sebastian Rojas <sebastianrojas812@gmail.com>
2022-11-03 16:18:29 -0400

    robofriends with redux

---
+++ src/actions.js
@@ -0,0 +1,31 @@
+import {
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED,
+    REQUEST_ROBOTS_PENDING
+
+} from './constants.js'
+
+import { apiCall } from './api/api.js';
+
+export const setSearchField = (text) => ({
+    type: CHANGE_SEARCH_FIELD,
+    payload: text
+
+})
+
+export const requestRobots = () => (dispatch) => {
+    dispatch({
+        type: REQUEST_ROBOTS_PENDING
+    });
+    apiCall('https://jsonplaceholder.typicode.com/users')
+        .then(data => dispatch({
+            type: REQUEST_ROBOTS_SUCCESS,
+            payload: data
+        }))
+        .catch(error => (dispatch({
+            type: REQUEST_ROBOTS_FAILED,
+            payload: error
+
+        })))
+}
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,110 +1,120 @@
-import React, { useEffect, useState} from 'react';
+import React, { /* useEffect, useState */ } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll.js';
 import './App.css';
+import { Component } from 'react'
+
+import { connect } from 'react-redux';
+import { setSearchField, requestRobots } from '../actions.js'
+
+// redux needed functions
+
+const mapStateToProps = (state) => {
+    return {
+        searchField: state.searchRobots.searchField,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error,
+        robots: state.requestRobots.robots
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}
+
+// Class component 
+class App extends Component {
+
+    componentDidMount() {
+        this.props.onRequestRobots()
+    }
+
+    render() {
+
+        const { searchField, onSearchChange, robots, isPending } = this.props
+
+        const filteredRobots = robots.filter((ele) => {
+
+            return ele.name.toLowerCase().includes(searchField.toLowerCase())
+        })
+        // for the if robots legnth is 0 - that would evaluate to false, bc 0 is false and 1 is true
+        //so we can use the ! in from of robots.length to clean up the code a bit
+
+        // the whole if else conditional can be converted into a ternary to clean up the code a bit
+        return (isPending) ?
+            <h1>Loading</h1> :
+
+            (
+                <div className='tc'>
+                    <h1 className='f1'>Robo-Friends!</h1>
+                    <SearchBox searchChange={onSearchChange} />
+                    <Scroll >
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+
+                </div>
+
+            );
+
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+
+
+
+
+
+///
 
 
 // functional and hooks component 
 
-const App = ()=>{
+// const App = () => {
 
-    // hooks 
+//     // hooks 
 
-    const [robots,setRobots] = useState([])
-    const [searchField,setSearchField] = useState('')
-    const [count,setCount] = useState(0)
+//     const [robots, setRobots] = useState([])
+//     const [searchField, setSearchField] = useState('')
+//     // const [count, setCount] = useState(0)
 
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=> response.json() )
-        .then(users => setRobots(users))
-
-    },[])
-   
-    const onSearchChange = (event)=>{
-        
-        setSearchField( event.target.value)
- 
-     }
-    
-     const filteredRobots = robots.filter((ele)=>{
-         return ele.name.toLowerCase().includes(searchField.toLowerCase())
-      })
-
-      return (!robots.length )? 
-      <h1>Loading</h1>:
-      
-      (
-         <div className='tc'>
-             <h1 className='f1'>Robo-Friends!</h1>
-             <button onClick={()=>setCount(count+1)}>Click Me</button>
-             <p>You have clicked {count} times</p>
-             <SearchBox searchChange = {onSearchChange} />
-             <Scroll >
-                 <CardList robots={ filteredRobots} />
-             </Scroll>
-              
-         </div>
-        
-     );
-}
-
-
-
-
-
-// Class component 
-// class App extends Component{
-//     constructor(){
-//         super()
-//         this.state = {
-//             robots: [],
-//             searchField: '',
-//         }
-        
-//     }
-
-//     componentDidMount(){
+//     useEffect(() => {
 //         fetch('https://jsonplaceholder.typicode.com/users')
-//         .then(response=> response.json() )
-//         .then(users=> this.setState({robots:users }));
+//             .then(response => response.json())
+//             .then(users => setRobots(users))
+
+//     }, [])
+
+//     const onSearchChange = (event) => {
+
+//         setSearchField(event.target.value)
+
 //     }
 
-//     onSearchChange = (event)=>{
-        
-//        this.setState({searchField : event.target.value})
+//     const filteredRobots = robots.filter((ele) => {
+//         return ele.name.toLowerCase().includes(searchField.toLowerCase())
+//     })
 
-        
-        
-//     }
+//     return (!robots.length) ?
+//         <h1>Loading</h1> :
 
-//     render(){
-//         const {robots, searchField} = this.state
-//         const filteredRobots = robots.filter((ele)=>{
-//             return ele.name.toLowerCase().includes(searchField.toLowerCase())
-//          })
-// // for the if robots legnth is 0 - that would evaluate to false, bc 0 is false and 1 is true
-// //so we can use the ! in from of robots.length to clean up the code a bit
+//         (
+//             <div className='tc'>
+//                 <h1 className='f1'>Robo-Friends!</h1>
 
-// // the whole if else conditional can be converted into a ternary to clean up the code a bit
-//         return (!robots.length )? 
-//              <h1>Loading</h1>:
-             
-//              (
-//                 <div className='tc'>
-//                     <h1 className='f1'>Robo-Friends!</h1>
-//                     <SearchBox searchChange = {this.onSearchChange} />
-//                     <Scroll >
-//                         <CardList robots={ filteredRobots} />
-//                     </Scroll>
-                     
-//                 </div>
-               
-//             );
-        
-        
-//     }
+
+//                 <SearchBox searchChange={onSearchChange} />
+//                 <Scroll >
+//                     <CardList robots={filteredRobots} />
+//                 </Scroll>
+
+//             </div>
+
+//         );
 // }
-
-export default App;
